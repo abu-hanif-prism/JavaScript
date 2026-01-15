5e5cbbb39eb5450d869da487dc988701c7aa4433
Abu Hanif <hanif.ov54@gmail.com>
2026-01-15 21:48:23 +0600

    first js lesson

---
+++ Website/index.js
@@ -0,0 +1,72 @@
+// console.log(`Hello`);
+// console.log(`I like pizza!`);
+// window.alert(`this is an alart!`)
+// window.alert(`I like pizza!`);
+
+// document.getElementById("myH1").textContent =`hello`;
+// document.getElementById(`myP`).textContent = `this is a peragraph`;
+//This is a comment
+/* this 
+    is 
+    a 
+    comment
+    */
+
+// let x = 150; 
+// let age =25;
+// let price = 100;
+// console.log(`you have $${price}`);
+// console.log(`you are ${age} years old`);
+// console.log(x);
+// //datatype
+// //string
+// let Fname = "hanif";
+// console.log(typeof Fname);
+// console.log(`My name is ${Fname}`);
+
+
+// // ___________________________________
+// let fullName ="Md Abu Hanif";
+// let age = "25";
+// let student = false ;
+
+// document.getElementById("p1").textContent = fullName;
+// document.getElementById("p2").textContent = age;
+// document.getElementById("p3").textContent = student;
+// document.getElementById(`p3`).textContent = `your name is ${fullName}. you are ${age} years old.`;
+
+
+//arithmetic operation 
+
+// let student = 30;
+// student = student+1;
+// student = student-1;
+// student = student*2;
+// student = student/2;
+// student = student**2;
+// student = student%3;
+//input >>>>>>>>>>>>>>>>>
+//window prompt
+// username =window.prompt("What is your username?");
+// console.log(username);
+
+
+// html text box>>>>>>>>>
+// let username;
+
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     document.getElementById("myH1").textContent = `hello ${username}`;
+// }
+
+
+//type conversion >>>>>>>>>
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+console.log(x,y,z,typeof x, typeof y , typeof z);
